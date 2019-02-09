930d70b1216c3af25876429520c8756fd358154c
Simon Zolin <stsaz@yandex.ru>
2019-02-09 10:01:38 +0300

    + ARM: ffatom_fence_*()

--- FFOS/cpu-arm.h
+++ FFOS/cpu-arm.h
@@ -20,5 +20,21 @@ static FFINL int ffatom_cmpset(ffatomic *a, size_t old, size_t newval)
 	return __sync_bool_compare_and_swap((volatile size_t*)&a->val, old, newval);
 }
 
+#if defined __aarch64__
+
+#define _ff_dmb(opt)  __asm volatile("dmb " #opt : : : "memory")
+#define ffatom_fence_acq()  _ff_dmb(ld)
+#define ffatom_fence_rel()  _ff_dmb(sy)
+#define ffatom_fence_acq_rel()  _ff_dmb(sy)
+#define ffatom_fence_seq_cst()  _ff_dmb(sy)
+
+#else
+
+#define ffatom_fence_acq()  __sync_synchronize()
 #define ffatom_fence_rel()  __sync_synchronize()
+#define ffatom_fence_acq_rel()  __sync_synchronize()
+#define ffatom_fence_seq_cst()  __sync_synchronize()
+
+#endif
+
 #define ffcpu_pause()
