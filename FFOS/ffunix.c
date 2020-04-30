b30e107f1ddae0f521f49391451608572132ceb0
Simon Zolin <stsaz@yandex.ru>
2020-04-30 07:46:25 +0300

    * unix: move ffnetconf functions

--- FFOS/ffunix.c
+++ FFOS/ffunix.c
@@ -597,23 +597,3 @@ ffskt ffaio_pipe_accept(ffkevent *kev, ffkev_handler handler)
 	}
 	return sk;
 }
-
-
-void ffnetconf_destroy(ffnetconf *nc)
-{
-	ffmem_free0(nc->dns_addrs);
-}
-
-extern int _ffnetconf_getdns(ffnetconf *nc);
-
-int ffnetconf_get(ffnetconf *nc, uint flags)
-{
-	int rc = -1;
-	switch (flags) {
-	case FFNETCONF_DNS_ADDR: {
-		rc = _ffnetconf_getdns(nc);
-		break;
-	}
-	}
-	return rc;
-}
