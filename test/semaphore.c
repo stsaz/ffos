368980da014d3c00eae5d2fa01429e0331033a23
Simon Zolin <stsaz@yandex.ru>
2020-08-30 09:12:50 +0300

    * semaphore.h

--- test/semaphore.c
+++ test/semaphore.c
@@ -8,11 +8,21 @@
 void test_semaphore_unnamed()
 {
 	ffsem s;
-	x(FFSEM_INV != (s = ffsem_open(NULL, 0, 0)));
+	x_sys(FFSEM_NULL != (s = ffsem_open(NULL, 0, 0)));
 	x(0 != ffsem_wait(s, 0));
 	x(fferr_last() == ETIMEDOUT);
+
+	x_sys(0 == ffsem_post(s));
+	x_sys(0 == ffsem_post(s));
+	// cnt=2
+
+	x(0 == ffsem_wait(s, -1));
+	x(0 == ffsem_wait(s, 0));
+	// cnt=0
+	x(0 != ffsem_wait(s, 0));
+
 	ffsem_close(s);
-	s = FFSEM_INV;
+	s = FFSEM_NULL;
 	x(0 != ffsem_wait(s, 0));
 }
 
@@ -23,22 +33,26 @@ void test_semaphore_named()
 
 	ffsem_unlink(name);
 
-	x(FFSEM_INV == ffsem_open(name, 0, 0));
-	x(FFSEM_INV != (s = ffsem_open(name, FFFILE_CREATENEW, 2)));
-	x(FFSEM_INV != (s2 = ffsem_open(name, 0, 0)));
-	x(0 == ffsem_wait(s, 0));
-	x(0 == ffsem_wait(s2, 0));
-	//cnt=0
+	x_sys(FFSEM_NULL == ffsem_open(name, 0, 0));
+	x_sys(FFSEM_NULL != (s = ffsem_open(name, FFSEM_CREATENEW, 2)));
+	x_sys(FFSEM_NULL != (s2 = ffsem_open(name, 0, 0)));
+	x_sys(0 == ffsem_wait(s, 0));
+	x_sys(0 == ffsem_wait(s2, 0));
+	// cnt=0
 	x(0 != ffsem_wait(s, 0) && fferr_last() == ETIMEDOUT);
 	x(0 != ffsem_wait(s2, 0) && fferr_last() == ETIMEDOUT);
 
-	x(0 == ffsem_post(s));
-	x(0 == ffsem_post(s2));
-	//cnt=2
+	x_sys(0 == ffsem_post(s));
+	x_sys(0 == ffsem_post(s2));
+	// cnt=2
 
-	x(0 == ffsem_wait(s, 0));
-	x(0 == ffsem_wait(s2, 0));
-	//cnt=0
+#ifdef FF_APPLE
+	x_sys(0 == ffsem_wait(s, 0));
+#else
+	x_sys(0 == ffsem_wait(s, 1000));
+#endif
+	x_sys(0 == ffsem_wait(s2, 0));
+	// cnt=0
 	x(0 != ffsem_wait(s, 100));
 	x(0 != ffsem_wait(s2, 0));
 
